3aa9a021d600c7643685f2f323c2e74dab04c353
hyperGT <charlesgabriel102@gmail.com>
2023-04-05 13:56:43 -0300

    Unchecked Exception - tratando excessões com try-catch

--- src/Unchecked_Exception.java
+++ src/Unchecked_Exception.java
@@ -3,19 +3,32 @@ import javax.swing.*;
 public class Unchecked_Exception {
 
     public static void main(String[] args) throws Exception {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
 
-        try {
 
-        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        // excessões possiveis: Numerador ou denominador serem informados como Strings ou tentar dividir o numerador por zero
+        boolean continueLooping = true;
+        do {
 
-        }catch (NumberFormatException e){
-            e.printStackTrace();
-        } finally {
-            System.out.println("Finally");
-        }
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+
+                continueLooping = false;
+
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Insira um valor válido! ");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossivel dividir por 0! ");
+            } finally {
+                System.out.println("Finally");
+            }
+        } while (continueLooping);
 
 
         System.out.println("O código continua...");
